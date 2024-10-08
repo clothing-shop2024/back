e890ee19cf691a8d9a5c82811d559d36915a32eb
hhhunnn <gusdk2090@naver.com>
2024-10-08 23:24:58 +0900

    cloth database와 entity 수정

--- src/main/java/com/project/back/entity/ClothColorMapEntity.java
+++ src/main/java/com/project/back/entity/ClothColorMapEntity.java
@@ -1,31 +1,32 @@
 package com.project.back.entity;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "cloth")
-@Table(name = "cloth")
+@Entity(name = "clothColorMap")
+@Table(name = "cloth_color_map")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ClothEntity {
-    
-    @Id
-    private String clothNumber;
-    private String clothName;
-    private String clothDate;
-    private Integer stock;
+public class ClothColorMapEntity {
 
     @ManyToOne
-    @JoinColumn(name = "cloth_detail_number")
-    private String clothDetailNumber;
+    @MapsId("clothNumber")
+    @JoinColumn(name = "cloth_number")
+    private String clothNumber;
+
+    @ManyToOne
+    @MapsId("colorNumber")
+    @JoinColumn(name = "color_number")
+    private int colorNumber;
 
 }
