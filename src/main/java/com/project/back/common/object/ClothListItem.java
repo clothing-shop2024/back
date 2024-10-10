9ad3de9794f6c397dc0611eb840bbd721cab5081
hhhunnn <gusdk2090@naver.com>
2024-10-11 04:12:46 +0900

    find-id, find-password 완료

--- src/main/java/com/project/back/common/object/ClothListItem.java
+++ src/main/java/com/project/back/common/object/ClothListItem.java
@@ -12,16 +12,12 @@ import lombok.Getter;
 public class ClothListItem {
     
     private String clothNumber;
-    private String clothName;
-    private String clothDate;
     private Integer stock;
     private String clothDetailNumber;
 
     private ClothListItem(ClothEntity clothEntity) throws Exception {
 
         this.clothNumber = clothEntity.getClothNumber();
-        this.clothName = clothEntity.getClothName();
-        this.clothDate = clothEntity.getClothDate();
         this.stock = clothEntity.getStock();
         // this.clothDetailNumber = ClothDetailEntity.getClothDetailNumber();
 
