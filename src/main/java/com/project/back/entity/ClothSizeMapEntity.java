3e59f7e288eb6511d74b65bd6f05f3bbb911af43
hhhunnn <gusdk2090@naver.com>
2024-10-10 21:35:49 +0900

    cloth entity error해결

--- src/main/java/com/project/back/entity/ClothSizeMapEntity.java
+++ src/main/java/com/project/back/entity/ClothSizeMapEntity.java
@@ -2,6 +2,7 @@ package com.project.back.entity;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
@@ -19,16 +20,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ClothSizeMapEntity {
     
-    @EmbeddedId
-    private Integer id;
-
     @ManyToOne
-    @MapsId("clothNumber")
+    @Id
     @JoinColumn(name = "cloth_number")
     private ClothEntity clothNumber;
 
     @ManyToOne
-    @MapsId("sizeNumber")
+    @Id
     @JoinColumn(name = "size_number")
     private ClothSizeEntity sizeNumber;
 
