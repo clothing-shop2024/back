3b6154c7c7bc6a5db926b2aaa28c3cacf803a663
hhhunnn <gusdk2090@naver.com>
2024-11-17 23:58:13 +0900

    admin-page user list search

--- src/main/java/com/project/back/controller/AdminController.java
+++ src/main/java/com/project/back/controller/AdminController.java
@@ -3,7 +3,9 @@ package com.project.back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.back.dto.response.user.GetAdminUserListResponseDto;
@@ -39,4 +41,38 @@ public class AdminController {
 
         return response;
     }
+
+    // 회원관리 아이디로 검색하기
+    @GetMapping("/list/userId/search")
+    public ResponseEntity<? super GetAdminUserListResponseDto> getUserIdSearchList (
+        @AuthenticationPrincipal String userId,
+        @RequestParam("word") String word
+    ) {
+        ResponseEntity<? super GetAdminUserListResponseDto> response = adminService.getUserIdSearchList(userId, word);
+
+        return response;
+    }
+
+    // 회원관리 이름으로 검색하기
+    @GetMapping("/list/userName/search")
+    public ResponseEntity<? super GetAdminUserListResponseDto> getUserNameSearchList (
+        @AuthenticationPrincipal String userId,
+        @RequestParam("word") String word
+    ) {
+        ResponseEntity<? super GetAdminUserListResponseDto> response = adminService.getUserNameSearchList(userId, word);
+
+        return response;
+    }
+
+    // 회원관리 등급으로 검색하기
+    @GetMapping("/list/grade/search")
+    public ResponseEntity<? super GetAdminUserListResponseDto> getUserGradeSearchList (
+        @AuthenticationPrincipal String userId,
+        @RequestParam("word") String word
+    ) {
+        ResponseEntity<? super GetAdminUserListResponseDto> response = adminService.getUserGradeSearchList(userId, word);
+
+        return response;
+    }
+
 }
